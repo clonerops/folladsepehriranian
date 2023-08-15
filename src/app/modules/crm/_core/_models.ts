25b9e1a6b86c31feabceaadff8a85afdc66fcada
iamabolfazlmasoumi <iamabolfazlmasoumi@gmail.com>
2023-08-15 17:07:32 +0330

    remove saipa dashboard

--- src/app/modules/crm/_core/_models.ts
+++
@@ -1,4 +0,0 @@
-export interface ComplaintOrRequestSendData {
-  fromDate: string
-  toDate: string
-}
--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -1,9 +1,6 @@
 import {Route, Routes, Navigate} from 'react-router-dom'
 import {MasterLayout} from '../../_cloner/layout/MasterLayout'
-import { DashboardEsale } from '../pages/dashboard/DashboardEsale'
 import Dashboard from '../pages/dashboard/Dashboard'
-import { DashboardTransfer } from '../pages/dashboard/DashboardTransfer'
-import { DashboardCrm } from '../pages/dashboard/DashboardCrm'
 
 const PrivateRoutes = () => {
 
@@ -11,12 +8,9 @@ const PrivateRoutes = () => {
     <Routes>
       <Route element={<MasterLayout />}>
         {/* Redirect to Dashboard after success login/registartion */}
-        <Route path='auth/*' element={<Navigate to='/dashboard/esale' />} />
+        <Route path='auth/*' element={<Navigate to='/dashboard' />} />
         {/* Pages */}
         <Route path='dashboard' element={<Dashboard />} />
-        <Route path='dashboard/esale' element={<DashboardEsale />} />
-        <Route path='dashboard/transfer' element={<DashboardTransfer />} />
-        <Route path='dashboard/crm' element={<DashboardCrm />} />
         {/* Lazy Modules */}
         {/* Page Not Found */}
         <Route path='*' element={<Navigate to='/error/404' />} />

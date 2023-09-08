9566c9432c7e15b53f6157fe685c2961564c5472
clonerops <114849678+clonerops@users.noreply.github.com>
2023-09-08 00:07:53 -0700

    dashboard page responsive

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
     return (
         <>
-            <div className="tw-grid tw-grid-cols-3 tw-gap-4">
+            <div className="tw-grid tw-grid-cols-1 md:tw-grid-cols-3 tw-gap-4">
                 <Link
-                    to="/dashboard/order"
+                    to="/dashboard/order/managment"
                     className="tw-rounded-lg tw-bg-indigo-600 tw-flex tw-justify-center tw-items-center tw-bg-gradient-to-r tw-from-indigo-500 tw-via-purple-500 tw-to-pink-500 tw-cursor-pointer"
                 >
                     <span className="tw-text-white tw-px-8 tw-py-12 tw-font-yekan_bold">
@@ -13,7 +13,7 @@ const Dashboard = () => {
                     </span>
                 </Link>
                 <Link
-                    to="/dashboard/crm"
+                    to="/dashboard/order/lists"
                     className="tw-rounded-lg tw-bg-indigo-600 tw-flex tw-justify-center tw-items-center tw-bg-gradient-to-r tw-from-indigo-500 tw-from-10% tw-via-sky-500 tw-via-30% tw-to-emerald-500 tw-to-90% tw-cursor-pointer"
                 >
                     <span className="tw-text-white tw-px-8 tw-py-12 tw-font-yekan_bold">
